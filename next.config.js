dff9f841b5facd1182081e92cca7e043bd1924da
Samir Diaz <samir.diaz.1206@gmail.com>
2022-01-31 22:40:31 -0500

    se elimino prefix de export para prueba con gitpages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? './' : ''
+  // assetPrefix: isProd ? './' : ''
 }
